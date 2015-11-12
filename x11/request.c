15ab373f02f0722e97b82505cbab6efcd52407de
guodong <gd@tongjo.com>
2015-11-12 12:16:26 +0000

    d

--- x11/request.c
+++ x11/request.c
@@ -7,4 +7,5 @@ void cdp_x11_request_window_move(u32 wid, i16 x, i16 y)
 {
     u32 values[] = {x, y};
     xcb_configure_window(xconn, wid, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, values);
+    xcb_flush(xconn); 
 }
