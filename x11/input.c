94cecbbe43c9365bcc9bae10ae6d52c6da06b5d9
guodong <gd@tongjo.com>
2015-11-13 03:25:11 +0000

    d

--- x11/input.c
+++ x11/input.c
@@ -23,18 +23,18 @@ void cdp_x11_input_mouseup(u32 wid, u8 code)
 
 void cdp_x11_input_keydown(u32 wid, u8 code)
 {
-    xcb_key_symbols_t *syms = NULL;
-    xcb_keycode_t kc;
-    xcb_keysym_t ks = code;
-    kc = xcb_key_symbols_get_keycode(syms, ks);
-    xcb_test_fake_input(xconn, XCB_KEY_PRESS, kc, XCB_CURRENT_TIME, wid, 0, 0, 0 );
+    // xcb_key_symbols_t *syms = NULL;
+    // xcb_keycode_t kc;
+    // xcb_keysym_t ks = code;
+    // kc = xcb_key_symbols_get_keycode(syms, ks);
+    xcb_test_fake_input(xconn, XCB_KEY_PRESS, code, XCB_CURRENT_TIME, wid, 0, 0, 0 );
 }
 
 void cdp_x11_input_keyup(u32 wid, u8 code)
 {
-    xcb_key_symbols_t *syms = NULL;
-    xcb_keycode_t kc;
-    xcb_keysym_t ks = code;
-    kc = xcb_key_symbols_get_keycode(syms, ks);
-    xcb_test_fake_input(xconn, XCB_KEY_RELEASE, kc, XCB_CURRENT_TIME, XCB_NONE, 0, 0, 0 );
+    // xcb_key_symbols_t *syms = NULL;
+    // xcb_keycode_t kc;
+    // xcb_keysym_t ks = code;
+    // kc = xcb_key_symbols_get_keycode(syms, ks);
+    xcb_test_fake_input(xconn, XCB_KEY_RELEASE, code, XCB_CURRENT_TIME, XCB_NONE, 0, 0, 0 );
 }
