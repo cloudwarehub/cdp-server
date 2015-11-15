c16cf9f7cc66fd8e610e1c5e5a1b67fc04940d67
guodong <gd@tongjo.com>
2015-11-15 16:49:00 +0000

    fix x11 resize

--- cdp_request.c
+++ cdp_request.c
@@ -2,6 +2,7 @@
 
 void cdp_request_window_resize(cdp_request_window_resize_t *req)
 {
+    cdp_x11_request_window_resize(req->wid, req->width, req->height);
     cdp_window_resize(req->wid, req->width, req->height);
     cdp_stream_resize(req->wid, req->width, req->height);
 }
--- x11/request.c
+++ x11/request.c
@@ -9,3 +9,10 @@ void cdp_x11_request_window_move(u32 wid, i16 x, i16 y)
     xcb_configure_window(xconn, wid, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, values);
     xcb_flush(xconn); 
 }
+
+void cdp_x11_request_window_resize(u32 wid, i16 width, i16 height)
+{
+    u32 values[] = {width, height};
+    xcb_configure_window(xconn, wid, XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT, values);
+    xcb_flush(xconn); 
+}
